d71a610aac5f358c99668a57c64b098e5b7701c5
neamatullahmustafa <tareqshrouq@gmail.com>
2024-03-13 22:00:37 +0200

    merge with neamaTullah

--- React/src/App.js
+++ React/src/App.js
@@ -3,42 +3,43 @@ import "./App.css";
 import LayOut from "./components/LayOut/LayOut.jsx";
 import {
   // BrowserRouter, Routes, Route,
-  createBrowserRouter, RouterProvider
+  createBrowserRouter,
+  RouterProvider,
 } from "react-router-dom";
-import Categories from './components/Categories/Categories';
-import Details from './components/Details/Details';
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import Sell from './components/Sell/Sell';
-import ErrorPage from './components/ErrorPage/ErrorPage';
-import Notification from './components/Notification/Notification';
-import ProductItem from './components/ProductItem/ProductItem';
-import { Fragment } from 'react';
+import Categories from "./components/Categories/Categories";
+import Details from "./components/Details/Details";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Sell from "./components/Sell/Sell";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+import Notification from "./components/Notification/Notification";
+import ProductItem from "./components/ProductItem/ProductItem";
+import { Fragment } from "react";
 import CategoryProductItem from "./components/CategoryProductItem/CategoryProductItem.jsx";
 function App() {
-let routes = createBrowserRouter([
-  {
-    path: "",
-    element: <LayOut />,
-    children: [
-      { path: "Categories", element: <Categories /> },
-      { path: "Details", element: <Details /> },
-      { path: "Home", element: <Home /> },
-      { path: "Login", element: <Login /> },
-      { path: "Register", element: <Register /> },
-      { path: "Notification", element: <Notification /> },
-      { path: "*", element: <ErrorPage /> },
-    ],
-  },
-  { path: "sell", element: <Sell /> },
-]);
+  let routes = createBrowserRouter([
+    {
+      path: "",
+      element: <LayOut />,
+      children: [
+        { path: "Categories", element: <Categories /> },
+        { path: "Details", element: <Details /> },
+        { path: "Home", element: <Home /> },
+        { path: "Login", element: <Login /> },
+        { path: "Register", element: <Register /> },
+        { path: "Notification", element: <Notification /> },
+        { path: "*", element: <ErrorPage /> },
+      ],
+    },
+    { path: "sell", element: <Sell /> },
+  ]);
 
-  return (<Fragment>
-    <RouterProvider router={routes}></RouterProvider>
-    <CategoryProductItem/>
-  </Fragment>
-    
+  return (
+    <Fragment>
+      <RouterProvider router={routes}></RouterProvider>
+      <CategoryProductItem />
+    </Fragment>
   );
 }
 
