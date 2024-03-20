d253eeb9c9b86d9ae0e8b1430536bfadc69c44a9
William George <williamgeorge1997x@gmail.com>
2024-03-20 23:49:31 +0200

    Update

--- Laravel/app/Http/Controllers/StripeController.php
+++ Laravel/app/Http/Controllers/StripeController.php
@@ -51,28 +51,4 @@ class StripeController extends Controller
     }
 
 
-    public function storePayment(Request $request)
-    {
-        try {
-            $user = JWTAuth::parseToken()->authenticate();
-            $cart = Cart::where('user_id', $user->id)->first();
-
-            if (!$cart) {
-                return response()->json(['error' => 'Cart not found'], 404);
-            }
-
-            $cartProducts = CartProduct::where('cart_id', $cart->id)->get();
-            $totalCartPrice = $cartProducts->sum('total_product_price');
-
-            $payment = new Payment();
-            $payment->cart_id = $cart->id;
-            $payment->amount = $totalCartPrice;
-            $payment->save();
-
-            return response()->json(['message' => 'Payment stored successfully']);
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 500);
-        }
-    }
-
 }
